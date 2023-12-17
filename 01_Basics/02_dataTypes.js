b5a22f3b676f8a810fd6c11a0ce2d18c9f6aba9e
Sathwikshaga <shagasathwik@gmail.com>
2023-12-18 01:04:43 +0530

    added 02_dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treats all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Shaga")
+
+let username = "Sathwik"
+let age = 20
+let isLoggedIn = true
+let state;
+
+// number => range: 2^53
+// bigint is also used - for large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value datatype: object
+// difference b/w undefined and not defined
+// symbol => uniqueness
+
+// Object
+
+console.log(typeof age)
+console.log(typeof username)
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
