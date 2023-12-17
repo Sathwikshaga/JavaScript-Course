da66e1c28b20c90e072f495217d1c2f28f71ceb3
Sathwikshaga <shagasathwik@gmail.com>
2023-12-18 02:27:21 +0530

    strings done

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+const Name = "Sathwik"
+const repoCount = 11
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${Name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Cranky')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Sathwik   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('shyam'))
+
+console.log(gameName.split('-'));
